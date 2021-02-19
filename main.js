15300ddcb85fcae548a2e25d2344d0d0f1653942
John Meneses <jmmenes90@gmail.com>
2021-02-19 03:11:18 -0500

    Finished, passed all tests.

---
+++ main.js
@@ -0,0 +1,54 @@
+function addQuestionMarks(string) {
+  return string + "!!!";
+}
+const result1 = addQuestionMarks("hello");
+result1;
+const result2 = addQuestionMarks("what even");
+result2;
+
+function addSpace(string1, string2) {
+  return string1 + " " + string2;
+}
+const result3 = addSpace("oh", "hi");
+result3;
+const result4 = addSpace("well", "hello back");
+result4;
+
+function subtractThree(num1, num2, num3) {
+  return num1 - num2 - num3;
+}
+const result5 = subtractThree(10, 1, 2);
+result5;
+const result6 = subtractThree(8, 3, 5);
+result6;
+const result7 = subtractThree(100, 80, 25);
+result7;
+
+function reverseSubtractThree(num4, num5, num6) {
+  return num4 - num5 - num6;
+}
+
+const result8 = reverseSubtractThree(2, 1, 10);
+result8;
+const result9 = reverseSubtractThree(10, 1, 2);
+result9;
+
+function tripleString(string3) {
+  return string3 + string3 + string3;
+}
+
+const result10 = tripleString("codeimmersives");
+result10;
+const result11 = tripleString("is the best");
+result11;
+
+function squareRoot(num7) {
+  return Math.sqrt(num7);
+}
+
+const result12 = squareRoot(16);
+result12;
+const result13 = squareRoot(4);
+result13;
+const result14 = squareRoot(9);
+result14;
